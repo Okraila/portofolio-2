9ac424ed137621dfac3ceab72c980f94910121f9
Okraila <o.kraievskyi@gmail.com>
2023-08-01 20:36:17 +0300

    javascript

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const form=document.querySelector("form")
 // ключове слово ім'я змінної = значення
 // let, const, var - ключові слова.
-const varName = "var name"
-const varname = "var"
+const varName = "var name";
+const varname = "sd";
+
+const currentYear = 2023;
+console.log("Рік", currentYear);
+
+const num = 10; 
+const str = "string";
+const bool = true;
+const selectedProduct = null;
+let username;
+
+const x = 5;
+const y = 10;
+
+console.log([x < y]);
+
+let value = 9;
+value += 10;
+console.log(value);
+
+let value = 9;
+value += 10;
+
+const strr = "JavaScript";
+
+const message = "Banana" + "is" + "happy";
+console.log = (2+21);
+
+const fruit = "banana";
+const color = "yellow";
+
+const banana = `$(fruit) is $(color)`;
+console.log(fruit.toLowerCase);
+console.log(file.endsWith("js"));
